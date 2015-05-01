f7fff94592de469c2d6ec55765456ca2a3e8b5e1
1101137251 <qoo830525@gmail.com>
2015-05-01 17:17:03 +0800

    go

---
+++ KuasCore/Interceptors/DebogLogInterceptor.cs
@@ -0,0 +1,21 @@
+﻿using AopAlliance.Intercept;
+using System;
+using System.Diagnostics;
+
+namespace KuasCore.Interceptors 
+{
+    class DebogLogInterceptor : IMethodInterceptor
+    {
+
+        public object Invoke(IMethodInvocation invocation)
+        {
+
+
+            object result = invocation.Proceed();
+
+
+
+            return result;
+        }
+    }
+}
